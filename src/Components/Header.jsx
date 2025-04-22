3967653be9338eed8de8513e65ddf0b7bd9dd286
Bill <bill.omondi@student.moringaschool.com>
2025-04-22 15:12:55 +0300

    header-structure

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav className='header'>TITLE</nav>
+        <p>Welcome to </p>
+
+    </div>
+  )
+}
+
+export default Header
